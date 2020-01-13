f214763dfd59cb735efbf13efef06fdf6ff73a02
mohbadar <mohammadbadarhashimi@gmail.com>
2020-01-13 14:34:43 +0430

    cryptography utility library

---
+++ src/main/java/af/gov/anar/lib/cryto/AnarLibCryptoApplication.java
@@ -0,0 +1,13 @@
+package af.gov.anar.lib.cryto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnarLibCryptoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnarLibCryptoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/af/gov/anar/lib/cryto/AnarLibCryptoApplicationTests.java
@@ -0,0 +1,13 @@
+package af.gov.anar.lib.cryto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnarLibCryptoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
